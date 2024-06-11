1938a3b73e3d2d5bdeed6dbd23f10af61ded5b25
rudra-404 <rudrakaiser840@gmail.com>
2024-06-11 21:35:26 +0600

    PRE

---
+++ precision_test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+	printf("a = 10 \n");
+	printf("Enter a value = ");
+	scanf("%d", &num);
+	printf("The Final Value = %d", (10*num));
+	
+	return 0;
+}
