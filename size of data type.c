b0e31b84e76133f34f1c009a5923a50f1ff4a7c9
rudra-404 <rudrakaiser840@gmail.com>
2024-06-14 11:08:02 +0600

    size of data type

---
+++ size of data type.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("size of INTEGER data type is   : %d byte\n", sizeof(int));
+	printf("size of FLOAT data type is     : %d byte\n", sizeof(float));
+	printf("size of CHARACTER data type is : %d byte\n", sizeof(char));
+	printf("size of DOUBLE data type is    : %d byte", sizeof(double));
+	
+	return 0;
+}
