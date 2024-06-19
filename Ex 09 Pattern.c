acc915627979945cf4c9680439d38a40f0f0c2f3
rudra-404 <rudrakaiser840@gmail.com>
2024-06-19 10:48:10 +0600

    Ex 09

---
+++ Ex 09 Pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n, row, col;
+	
+	printf("Enter Lines Number : ");
+	scanf("%d", &n);
+	
+	for(row=1; row<=n; row++)
+	{
+		for(col=1; col<=(n-row)+1; col++)
+		{
+			printf("%d ",col);
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
