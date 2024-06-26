7de3bdfa33f3e7fc14982b6afd465d8fed1c0b5b
rudra-404 <rudrakaiser840@gmail.com>
2024-06-27 00:19:10 +0600

    Last 4 Digit Of ID

---
+++ Last Four Digit Of ID.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int test_case, i;
+	scanf("%d",&test_case);
+	for(i=1; i<=test_case; i++)
+	{
+		int ID, temp;
+		scanf("%d",&ID);
+		if(ID%2==0)
+		{
+			printf("%d\n",(((ID%10)*(ID%10))+(ID/1000)));
+		}
+		else
+		{
+			printf("%d\n",((((ID/10)%10)*((ID/10)%10))+(ID/1000)));
+		}
+	}
+	return 0;
+}
