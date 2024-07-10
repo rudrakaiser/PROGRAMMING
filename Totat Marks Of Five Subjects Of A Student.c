d71cf5e93fa148b5f6a4e9321b898e2618f65478
rudra-404 <rudrakaiser840@gmail.com>
2024-07-10 14:42:16 +0600

    Five Subjects Mark

--- Totat Marks Of Five Subjects Of A Student.c
+++ Totat Marks Of Five Subjects Of A Student.c
@@ -2,11 +2,13 @@
 int main()
 {
     int n, i, j, sum, a;
+    printf("Total Studen = ");
     scanf("%d",&n);
     for(i=1; i<=n; i++)
     {
     	sum=0;
     	a=0;
+    	printf("Enter all marks = ");
 		for(j=1; j<=5; j++)
     	{
     		scanf("%d",&a);
